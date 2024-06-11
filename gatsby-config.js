8996566c625a947f1dfef073e745b9311a483f4a
Prarup Gurung <grgprarup@gmail.com>
2024-06-11 12:34:28 +0545

    Project setup

---
+++ gatsby-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: "portfolio-node",
+      options: {
+        siteUrl: "https://prarupgurung.netlify.app/", // Used for sitemap generation
+        manifestSettings: {
+          favicon: "PG", // Path is relative to the root
+          siteName: "My Portfolio", // Used in manifest.json
+          shortName: "Portfolio", // Used in manifest.json
+          startUrl: "/", // Used in manifest.json
+          backgroundColor: "#FFFFFF", // Used in manifest.json
+          themeColor: "#000000", // Used in manifest.json
+          display: "minimal-ui", // Used in manifest.json
+        },
+        contentDirectory: "./content",
+        blogSettings: {
+          path: "/blog", // Defines the slug for the blog listing page
+          usePathPrefixForArticles: false, // Default true (i.e. path will be /blog/first-article)
+        },
+        // googleAnalytics: {
+        //     trackingId: "UA-XXXXXX-X",
+        //     anonymize: true, // Default true
+        //     environments: ["production", "development"] // Default ["production"]
+        // }
+      },
+    },
+  ],
+};
